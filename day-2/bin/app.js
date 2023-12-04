df90cf93169d7dee2f244750affee7060fe502ab
J4Numbers <j4numbers@gmail.com>
2023-12-04 17:35:20 +0000

    docs(advent): add comments to days 1-4
    Signed-off-by: J4Numbers <j4numbers@gmail.com>

--- day-2/bin/app.js
+++ day-2/bin/app.js
@@ -43,6 +43,8 @@ const args = argsEngine.wrap(argsEngine.terminalWidth())
   .parse();
 
 const debug = args.debug;
+
+// Set the allowed maximums as required
 const allowedCubeMax = {
   red: args.maxRed,
   blue: args.maxBlue,
@@ -51,29 +53,50 @@ const allowedCubeMax = {
 
 let count = 0;
 let minPowersTotal = 0;
+
+// Set up some basic regex splits to split out individual games and pulls and colours from
+// each line
 const gameRegex = new RegExp('^game ([0-9]+): ((([^;]+)(;|\n)?)+)$', 'i');
 const gameSplitterRegex = new RegExp('(([0-9]+ green|[0-9]+ red|[0-9]+ blue)+,? ?)+(;|$)', 'ig');
 const pullSplitterRegex = new RegExp('([0-9]+) (green|red|blue)', 'ig');
 
+/**
+ * @param line - Print to console if debug is enabled
+ */
 const debugLine = (line) => {
   if (debug) {
     console.log(line);
   }
 };
 
+/**
+ * Given a line which describes a game made up of several rounds, where each round contains
+ * a set of tuples, containing the number and colour of cubes that were found, we want to
+ * calculate the minimum required cubes of each colour to be in this bag for this game to
+ * have been possible.
+ *
+ * @param line - A line containing inforamtion about a single game made up of several rounds
+ * @returns an object of the minimum required red, green, and blue cubes to have played this
+ * game
+ */
 const calculateGameOutcome = (line) => {
   let gameId = 0;
   let maxRed = 0;
   let maxBlue = 0;
   let maxGreen = 0;
   const gameDetails = gameRegex.exec(line);
+  // If this line contains a valid game...
   if (gameDetails) {
     let onePull;
     gameId = Number(gameDetails[1]);
+    // Split the game into rounds
     while ((onePull = gameSplitterRegex.exec(gameDetails[2])) !== null) {
       let oneColour;
+      // Split the round into individual colours
       while ((oneColour = pullSplitterRegex.exec(onePull[0])) !== null) {
         const cubeCount = Number(oneColour[1]);
+        // If there is more of a given colour than we have already seen, then update the
+        // required minimum of that colour accordingly
         switch (oneColour[2]) {
           case 'red':
             if (cubeCount > maxRed) {
@@ -102,20 +125,41 @@ const calculateGameOutcome = (line) => {
   };
 };
 
+/**
+ * Given a reported game, return whether that game is even possible if we have a set
+ * number of available cubes.
+ *
+ * @param maxDiscoveredCubes - The minimum cubes of a given colour to have played a
+ *                             given game
+ * @param maxAllowedCubes - The known maximum number of cubes that we have available
+ *                          in each colour
+ * @returns true if there were more (or the same amount of) allowed cubes of every
+ * colour than discovered cubes
+ */
 const testGameValid = (maxDiscoveredCubes, maxAllowedCubes) => {
   return !((maxDiscoveredCubes.red > maxAllowedCubes.red)
     || (maxDiscoveredCubes.blue > maxAllowedCubes.blue)
     || (maxDiscoveredCubes.green > maxAllowedCubes.green));
-}
+};
 
+// MAIN CODE STARTS HERE
+
+// For each line in the provided file...
 const file = fs.readFileSync(args.input).toString('utf-8');
 file.split('\n')
   .forEach((line) => {
+    // Calculate the minimum required cubes to play this game
     const discoveredCubeMax = calculateGameOutcome(line);
     if (discoveredCubeMax.gameId > 0) {
+      // Generate the power value of the cubes in this game
       let minCubesPower = (discoveredCubeMax.red * discoveredCubeMax.green * discoveredCubeMax.blue);
       minPowersTotal += minCubesPower;
+
+      // Set up a base debug log template
       const baseGameLog = `Game ${discoveredCubeMax.gameId} - Red ${discoveredCubeMax.red}/${allowedCubeMax.red} - Blue ${discoveredCubeMax.blue}/${allowedCubeMax.blue} - Green ${discoveredCubeMax.green}/${allowedCubeMax.green} - Power ${minCubesPower} - Total power ${minPowersTotal}`;
+
+      // Debug differently depending on whether this game is valid according to our set amount
+      // of allowed cubes
       if (testGameValid(discoveredCubeMax, allowedCubeMax)) {
         count += discoveredCubeMax.gameId;
         debugLine(`${baseGameLog} - VALID - Count ${count}`);
@@ -125,6 +169,7 @@ file.split('\n')
     }
   });
 
+// Print end results
 console.log(`Powers - ${minPowersTotal}`);
 console.log(`Valid count - ${count}`);
 
--- day-4/bin/app.js
+++ day-4/bin/app.js
@@ -26,18 +26,34 @@ const args = argsEngine.wrap(argsEngine.terminalWidth())
 
 const debug = args.debug;
 
+// Set up all working variables
 let validCards = [];
 let copyCount = {};
 let maxLines = 0;
 let count = 0;
+
+// Set a split regex to partition each line into the card ID, the winning numbers,
+// and the available numbers
 const cardSplitRegex = new RegExp('^card *([0-9]+): +([0-9 ]+) +[|] +([0-9 ]+)$', 'i');
 
+/**
+ * @param line - Print this line if debug is enabled
+ */
 const debugLine = (line) => {
   if (debug) {
     console.log(line);
   }
 };
 
+/**
+ * For a given line, calculate how many matches there are on this card between the
+ * set of winning numbers and the set of available numbers. This function returns
+ * that data as an object with multiple distinct items of information.
+ *
+ * @param line - The line that we're finding matches on
+ * @returns An object detailing the winning, discovered, and matched numbers within
+ * a single card
+ */
 const calculateMatches = (line) => {
   let cardId = 0;
   let winningNumbers = [];
@@ -58,6 +74,19 @@ const calculateMatches = (line) => {
   };
 };
 
+/**
+ * Given an original card ID from which we are triggering copies of subsequent
+ * cards and the number of matching numbers on that card, update our copy map with
+ * the new numbers of required copies of that card.
+ *
+ * Note: If the original card ID has a number of copies already linked against it,
+ * then that number of copies is added to every copy that this card requests
+ *
+ * @param originalCardId - The ID of the card from which we are springing copies
+ *                         from
+ * @param matchingCount - The number of subsequent cards that need an additional
+ *                        copy, or set of copies
+ */
 const markCopies = (originalCardId, matchingCount) => {
   let additionValue = 1;
   if (Object.keys(copyCount).includes(`${originalCardId}`)) {
@@ -73,30 +102,43 @@ const markCopies = (originalCardId, matchingCount) => {
   }
 };
 
+// MAIN CODE STARTS HERE
+
+// Split a file on new lines and iterate through all of them - noting down the maximum
+// number of available lines that there are
 const file = fs.readFileSync(args.input).toString('utf-8');
 const lines = file.split('\n');
 maxLines = lines.length;
 lines.forEach((line) => {
-    const foundMatches = calculateMatches(line);
-    if (foundMatches.cardId > 0) {
-      validCards.push(foundMatches.cardId);
-      const matchCount = foundMatches.matchedNumbers.length;
-      markCopies(foundMatches.cardId, matchCount);
-      let cardVal = 0;
-      if (matchCount > 0) {
-        cardVal = Math.pow(2, matchCount - 1);
-      }
-      count += cardVal;
-      const copyStatement = Object.keys(copyCount).includes(`${foundMatches.cardId}`)
-        ? `${copyCount[foundMatches.cardId]} copies`
-        : `0 copies`;
-      debugLine(`Card ${foundMatches.cardId} - ${matchCount} matches - ${copyStatement} - Value ${cardVal} - Total ${count}`);
-    }
-  });
+  // Calculate the matches found on each line
+  const foundMatches = calculateMatches(line);
+  if (foundMatches.cardId > 0) {
+    // If the match was valid, count this as a valid card, and mark the subsequent cards
+    // for copies if there were any matches
+    validCards.push(foundMatches.cardId);
+    const matchCount = foundMatches.matchedNumbers.length;
+    markCopies(foundMatches.cardId, matchCount);
 
+    // Calculate the value of this card
+    let cardVal = 0;
+    if (matchCount > 0) {
+      cardVal = Math.pow(2, matchCount - 1);
+    }
+    count += cardVal;
+
+    // Debug the value of this card and the number of copies
+    const copyStatement = Object.keys(copyCount).includes(`${foundMatches.cardId}`)
+      ? `${copyCount[foundMatches.cardId]} copies`
+      : `0 copies`;
+    debugLine(`Card ${foundMatches.cardId} - ${matchCount} matches - ${copyStatement} - Value ${cardVal} - Total ${count}`);
+  }
+});
+
+// Calculate the total number of scratchcards, including originals and copies
 const totalCards = Object.keys(copyCount)
   .filter((cardId) => validCards.includes(Number(cardId)))
   .reduce((totalCards, currentId) => totalCards + copyCount[currentId], validCards.length);
 
+// Log outputs
 console.log(`Valid count - ${count}`);
 console.log(`Total scratchcards - ${totalCards}`);
