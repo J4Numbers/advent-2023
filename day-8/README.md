ea7dd09f2a8c637bfb8a64fb9a3d180d2cdd4083
Jayne Doe <jayne.doe@engineering.digital.dwp.gov.uk>
2023-12-08 14:10:57 +0000

    feat(day-8): focus on part one of the question to follow a route
    Signed-off-by: Jayne Doe <jayne.doe@engineering.digital.dwp.gov.uk>

--- day-8/README.md
+++ day-8/README.md
@@ -45,6 +45,43 @@ ZZZ = (ZZZ, ZZZ)
 
 ## Part two
 
+Alternatively, instead of just starting from `AAA`, we instead start simultaneously from all nodes
+ending in `A`. The challenge now is to find the number of steps required before all nodes are
+ending in `Z`.
+
+> If some nodes are ending in `Z`, but not all of them, then we continue on all nodes until we
+> find a better match
+
+For example:
+
+```txt
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+```
+
+Here, there are two starting nodes, `11A` and `22A` (because they both end with A). As you follow
+each left/right instruction, use that instruction to simultaneously navigate away from both nodes
+you're currently on. Repeat this process until all of the nodes you're currently on end with `Z`.
+In this example, you would proceed as follows:
+
+* Step 0: You are at `11A` and `22A`.
+* Step 1: You choose all of the left paths, leading you to `11B` and `22B`.
+* Step 2: You choose all of the right paths, leading you to `11Z` and `22C`.
+* Step 3: You choose all of the left paths, leading you to `11B` and `22Z`.
+* Step 4: You choose all of the right paths, leading you to `11Z` and `22B`.
+* Step 5: You choose all of the left paths, leading you to `11B` and `22C`.
+* Step 6: You choose all of the right paths, leading you to `11Z` and `22Z`.
+
+So, in this example, you end up entirely on nodes that end in `Z` after 6 steps.
+
 ## This script
 
 This script uses Python and can be run with the following command:
@@ -53,4 +90,9 @@ This script uses Python and can be run with the following command:
 python pathfinder.py -i input.txt
 ```
 
-This will answer part one of the question as described above.
+This will answer part one of the question as described above. To switch to answering part two, add
+the `--mode haunt` parameter to the program. i.e.
+
+```bash
+python pathfinder.py -i input.txt --mode haunt
+```
--- day-8/pathfinder.py
+++ day-8/pathfinder.py
@@ -46,107 +46,47 @@ def debug_line(line_to_print):
         print(line_to_print)
 
 
-def flat_directions(so_far, current_node):
-    return so_far + current_node["direction"]
-
-
-def history_contains(history, test_node):
-    """
-    Test whether a given history array already contains a test node as
-    visited.
-    :param history: the complete journey from start to finish we have
-                    taken to reach this point.
-    :param test_node: the node to test the presence of in our history.
-    :returns: True if the node exists in our history
-    """
-    test_presence = list(filter(lambda point: point["node"] == test_node, history))
-    return len(test_presence) > 0
-
-
-def split_on_node(split_point, target_node, map_of_nodes, history):
-    """
-    Split a given node in the node map to follow both paths and construct an
-    eventual path to the target node. The rules are: no overlapping history
-    as it means backtracking, and the final node is it.
-
-    :param split_point: the node that we are splitting down the middle.
-    :param target_node: the final node we are aiming to reach.
-    :param map_of_nodes: the map of all nodes and edges that connect them.
-    :param history: the journey we have taken to reach this split node.
-    :return: A list of separate histories that are either final or terminated
-    """
-    histories = []
-    options = map_of_nodes[split_point]
-    dir_so_far = reduce(flat_directions, history, "");
-
-    if split_point == target_node:
-        history_point = {
-            "node": split_point,
-            "final": True
-        }
-        histories.append([*history, history_point])
-        debug_line(f"Target node {target_node} found after {len(history) + 1} hops - {dir_so_far}")
-
-    else:
-        avail_paths = 0
-
-        if not history_contains(history, options[0]) and options[0] not in redlisted_nodes:
-            hist_left = {
-                "node": split_point,
-                "direction": "L",
-                "next_node": options[0]
-            }
-            split_hist_l = split_on_node(options[0], target_node, map_of_nodes, [*history, hist_left])
-            histories += split_hist_l
-            if len(split_hist_l) == 0:
-                debug_line(f"Redlisting {options[0]} as no paths to victory were found")
-                redlisted_nodes.append(options[0])
-        else:
-            debug_line(f"Repeat L node {options[0]} found after {len(history)} hops - {dir_so_far}")
-
-        if not history_contains(history, options[1]) and options[1] not in redlisted_nodes:
-            hist_right = {
-                "node": split_point,
-                "direction": "R",
-                "next_node": options[1]
-            }
-            split_hist_r = split_on_node(options[1], target_node, map_of_nodes, [*history, hist_right])
-            histories += split_hist_r
-            if len(split_hist_r) == 0:
-                debug_line(f"Redlisting {options[1]} as no paths to victory were found")
-                redlisted_nodes.append(options[1])
-        else:
-            debug_line(f"Repeat R node {options[1]} found after {len(history)} hops - {dir_so_far}")
-
-    return histories
+def follow_path(start_nodes, target_nodes, map_of_nodes, directions):
+    working_node = start_nodes[0]
+    dir_idx = 0
+    hops = 0
+    while working_node not in target_nodes:
+        hops += 1
+        prev_node = working_node
+        working_node = map_of_nodes[working_node][0 if directions[dir_idx] == "L" else 1]
+        debug_line(f"Going {directions[dir_idx]} from {prev_node} to {working_node} - {hops}")
+        dir_idx = (dir_idx + 1) % len(directions)
+    return hops
 
 
 # MAIN CODE STARTS HERE
 
 # Set up some base variables
+dir_line = []
 node_map = {}
-travel_history = []
-redlisted_nodes = []
 
 # For each line in the file, we extract the hand of cards and their points
 # that are associated with them and add them onto a working tuple array.
 if args.input:
     with open(args.input, encoding="utf-8") as fp:
         for line in fp:
+            if re.search(DIRECTIONS_REGEX, line, flags=re.IGNORECASE):
+                dir_line = list(line.strip())
             line_discovery = re.search(NODE_REGEX, line, flags=re.IGNORECASE)
             if line_discovery is not None:
                 node_map[line_discovery.group(1)] = (
                         line_discovery.group(2), line_discovery.group(3))
 
-# Explode all possible paths from the start to the target node
-paths = split_on_node(START_NODE, END_NODE, node_map, [])
+start_nodes = []
+end_nodes = []
 
-# Reduce all paths down to the winning path
-winning_path = reduce(
-    lambda winner, challenger: challenger if not winner else (winner if len(winner) < len(challenger) else challenger),
-    paths, None)
+if args.mode == "haunt":
+    start_nodes = filter(lambda n: n.endswith("A"), node_map.keys())
+    end_nodes = filter(lambda n: n.endswith("Z"), node_map.keys())
+else:
+    start_nodes = [START_NODE]
+    end_node = [END_NODE]
 
-print(f"{len(paths)} different paths from {START_NODE} to {END_NODE} were found")
-# Print out the final result
-print(winning_path)
-print(len(winning_path))
+path = follow_path(start_nodes, END_NODE, node_map, dir_line)
+print(path)
+
